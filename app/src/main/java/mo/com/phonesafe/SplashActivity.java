7cbf1e9218e968db5fa8f3143679ac4d06faa5da
momxmo <876164651@qq.com>
2015-08-28 18:14:59 +0800

    手机安全卫士 2015-08-28 18:14:51

---
+++ app/src/main/java/mo/com/phonesafe/SplashActivity.java
@@ -0,0 +1,14 @@
+package mo.com.phonesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
